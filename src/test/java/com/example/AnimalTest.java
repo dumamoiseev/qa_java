e8e71eead615e29f5c4305bd5e7588a74b6b0423
Dmitry Moiseev <duma.moiseev@gmail.com>
2021-11-05 13:46:56 +0300

    Sprint2_finised. PLS review.

---
+++ src/test/java/com/example/AnimalTest.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+
+
+public class AnimalTest {
+
+    private Animal animal;
+
+    @Before
+    public void initObjects() {
+        animal = new Animal();
+    }
+
+    @Test
+    public void getPredatorFoodTest() throws Exception {
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        Assert.assertTrue(animal.getFood("Хищник").containsAll(expectedFood));
+    }
+
+    @Test
+    public void getHerbivorousFoodTest() throws Exception {
+        List<String> expectedFood = List.of("Трава", "Различные растения");
+        Assert.assertTrue(animal.getFood("Травоядное").containsAll(expectedFood));
+    }
+
+    @Test(expected = Exception.class)
+    public void getFoodExceptionTest() throws Exception {
+        animal.getFood("Колибри");
+    }
+
+    @Test
+    public void getFamilyTest(){
+        Assert.assertEquals(animal.getFamily(),("Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи"));
+    }
+
+
+}
